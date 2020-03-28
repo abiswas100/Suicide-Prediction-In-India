2f2d9c174cbf711b9faccec03b29a4667e256547
Biswas <nivedita.biswas@my.und.edu>
2020-03-28 15:40:06 -0500

    Updated

---
+++ Naive Bayes/Naive Bayes on Education Level.py
@@ -0,0 +1,62 @@
+
+#imports
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.naive_bayes import GaussianNB 
+from sklearn.model_selection import train_test_split
+from sklearn import metrics 
+import os 
+
+
+'''
+education level encoding
+'''
+
+def lvl_encoding(x):
+    if(x == 'No Education'):
+        return 0
+    elif(x == 'Primary'):
+        return 1
+    elif(x == 'Middle'):
+        return 2
+    elif(x == 'Matriculate/Secondary'):
+        return 3
+    elif(x == 'Hr. Secondary/Intermediate/Pre-Universit'):
+        return 4
+    elif(x == 'Diploma'):
+        return 5
+    elif(x == 'Graduate'):
+        return 6
+    elif(x == 'Post Graduate and Above'):
+        return 7
+    else: return 8
+
+
+df = pd.read_csv("Education_Status.csv")
+
+cataegory = pd.Series([]) 
+for ind,row in df.iterrows():
+    df.loc[ind,"Cataegory"] = lvl_encoding(df.loc[ind,"Type"])
+df = df.astype({"Cataegory": int})
+
+df = df.drop(['State','Year','Type_code','Type','Age_group'],axis='columns')
+df = df[['Cataegory','Total','Gender']]
+
+
+X = df.drop(['Gender'],axis='columns')
+Y = df.Gender
+
+#Performing Naive bayes
+
+x_train, x_test, y_train, y_test = train_test_split(X,Y,test_size=0.1)
+training_data = [x_train,x_test]
+tseting_data = [y_train,y_test]
+
+gnb = GaussianNB() 
+gnb.fit(x_train, y_train) 
+
+y_pred = gnb.predict(x_test)
+print("Gaussian Naive Bayes model accuracy(in %):", metrics.accuracy_score(y_test, y_pred)*100)
+print("Finished.....")
+
--- Naive Bayes/Naive_Bayes_Social.py
+++ Naive Bayes/Naive_Bayes_Social.py
@@ -2,32 +2,50 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB 
+from sklearn.naive_bayes import GaussianNB
+from sklearn import metrics  
 import os
 
 
+'''
+SOcial Status Encoding
+'''
+def encoding(x):
+    if (x == 'Never Married'):
+        return 0
+    elif(x == 'Married'):
+        return 1
+    elif(x == 'Seperated'):
+        return 2
+    elif(x == 'Divorcee'):
+        return 3
+    elif (x == 'Widowed/Widower'):
+        return 4
+
+
 df = pd.read_csv("Social_Status.csv")
-df = df.drop(['State','Year','Type_code','Age_group'],axis = 1)
 
-df  = df['Type'].replace({
-    'Seperated' : 2,
-    'Widowed/Widower' : 4,
-    'Married' : 1, 
-    'Divorcee' : 3,
-    'Never Married' : 0
-}, inplace = True)
+cataegory = pd.Series([]) 
+for ind,row in df.iterrows():
+    df.loc[ind,"Cataegory"] = encoding(df.loc[ind,"Type"])
+df = df.astype({"Cataegory": int})
 
-print(df)
+
+df = df.drop(['State','Year','Type_code','Type','Age_group'],axis='columns')
+df = df[['Cataegory','Total','Gender']]
+
+
+X = df.drop(['Gender'],axis = 'columns')
+Y = df.Gender
 
 #Performing Naive Bayes
 
-# X = df.drop(['Gender'],axis = 1)
-# Y = df.Gender
+X_train1, X_test1, Y_train1, Y_test1 = train_test_split(X, Y, test_size=0.1) 
 
-# from sklearn.model_selection import train_test_split 
-# X_train1, X_test1, Y_train1, Y_test1 = train_test_split(X, Y, test_size=0.1) 
+gnb = GaussianNB()
+gnb.fit(X_train1,Y_train1)
 
-# gnb = GaussianNB()
-# gnb.fit(X_train1,Y_train1)
-
+y_pred = gnb.predict(X_test1)
+print("Gaussian Naive Bayes model accuracy(in %):", metrics.accuracy_score(Y_test1, y_pred)*100)
 print("Finished ......")
+
