2b879d7ce64448988ae957f1535f71f6192f5663
Avhishek <avhishekbiswas17@gmail.com>
2020-06-08 03:08:04 -0500

    Added Streamlit code

--- Regression/naive_bayes_pred.py
+++ Regression/naive_bayes_pred.py
@@ -7,11 +7,22 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn import metrics  
 import os
 import csv
-import streamlit
+import streamlit as st
 
 import new_Education as edu
 import new_Professional as prof
 import new_Social as social
+
+
+def main():
+    st.title("Suicide Prediction Model")
+    st.markdown("Lets use different models to predict Suicides in India")
+    
+    
+    
+if __name__ == "__main__":
+    main() 
+    
 
 
 
