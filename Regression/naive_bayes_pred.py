e9acb818181ec7024236ebf39599de82a04bd855
Avhishek <avhishekbiswas17@gmail.com>
2020-06-07 23:01:12 -0500

    Updated

---
+++ Regression/naive_bayes_pred.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import OneHotEncoder
+from sklearn import metrics  
+import os
+
+
